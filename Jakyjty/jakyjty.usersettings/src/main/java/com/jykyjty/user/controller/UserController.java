1c8ac09f04bf0e52016b00cf568fddb01da877d3
kate <Katrin.Schaak@gmx.de>
2024-09-02 10:39:30 +0200

    someCleanUp

--- Jakyjty/jakyjty.usersettings/src/main/java/com/jykyjty/user/controller/UserController.java
+++ Jakyjty/jakyjty.usersettings/src/main/java/com/jykyjty/user/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.jakyjty.restcontroller;
+package com.jykyjty.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;

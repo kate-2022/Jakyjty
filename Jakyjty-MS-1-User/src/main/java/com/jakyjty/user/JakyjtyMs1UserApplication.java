e9d7e6fa74696d5dcc9d8eb567db051daa9c5ca7
kate <Katrin.Schaak@gmx.de>
2024-09-06 15:58:18 +0200

    loggingAddedWhatHasChangedHere?NotLikeItUsedToBe..

--- Jakyjty-MS-1-User/src/main/java/com/jakyjty/user/JakyjtyMs1UserApplication.java
+++ Jakyjty-MS-1-User/src/main/java/com/jakyjty/user/JakyjtyMs1UserApplication.java
@@ -1,14 +1,37 @@
 package com.jakyjty.user;
 
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.spi.LoggerFactory;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import ch.qos.logback.core.ConsoleAppender;
+
+
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class JakyjtyMs1UserApplication {
+	
 
+		private static final Logger LOGGER = LogManager.getLogger(JakyjtyMs1UserApplication.class);
+	 
+		static {
+		SimpleLayout layout = new SimpleLayout();
+		ConsoleAppender appender = new ConsoleAppender(layout);
+		LOGGER.addAppender(appender);
+		LOGGER.setLevel(Level.ERROR);
+		}
+		
+		
 	public static void main(String[] args) {
+		LOGGER.debug("start of main method...");
 		SpringApplication.run(JakyjtyMs1UserApplication.class, args);
 	}
 

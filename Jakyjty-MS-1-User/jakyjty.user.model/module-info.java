a637054e7847d3e7d40ff71e7ddfb7e9f870c367
kate <Katrin.Schaak@gmx.de>
2024-09-08 21:23:59 +0200

    moreModuleIssues

--- Jakyjty-MS-1-User/jakyjty.user.model/module-info.java
+++ Jakyjty-MS-1-User/jakyjty.user.model/module-info.java
@@ -1,8 +1,7 @@
-module user.model.Module {
+module jakyjty.user.model.Module {
 	
-	requires module user.dao.Module;
+	requires module jakyjty.user.dao.Module;
 	//requires module exceptions.Module;
-	//requires module authentication.Module;
-	//requires module service.Module;
+	requires module jakyjty.session.Module;
 	uses IUserManagementService.name;
 }

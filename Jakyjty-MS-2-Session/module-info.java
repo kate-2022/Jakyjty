18c985ea2abef1016822755c377eaf4baedc132a
kate <Katrin.Schaak@gmx.de>
2024-09-09 10:33:42 +0200

    auth

--- Jakyjty-MS-2-Session/module-info.java
+++ Jakyjty-MS-2-Session/module-info.java
@@ -2,7 +2,6 @@ module jakyjty.session.Module{
 	
 	requires module jakyjty.user.dao.Module;
 	requires module jakyjty.user.model.Module;
-	//requires module login.Module;
 	//requires module exceptions.Module;
 	uses IUserManagementService.name;
 }

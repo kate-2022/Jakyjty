6b2e0ebf73916a0c8ae633db933d2f47bc3a9c35
kate <Katrin.Schaak@gmx.de>
2024-09-14 17:23:30 +0200

    exceptionAdded

--- Jakyjty-MS-1-User/jakyjty.user.dao/jakyjty.user.dao/module-info.java
+++ Jakyjty-MS-1-User/jakyjty.user.dao/jakyjty.user.dao/module-info.java
@@ -1,7 +1,7 @@
 module jakyjty.user.dao.Module {
 	
 	requires module jakyjty.user.model.Module;
-	//requires module exceptions.Module;
+	requires module exception.Module;
 	requires module jakyjty.session.Module;
 	uses IUserManagementService.name;
 }
--- Jakyjty-MS-1-User/jakyjty.user.model/jakyjty.user.model/module-info.java
+++ Jakyjty-MS-1-User/jakyjty.user.model/jakyjty.user.model/module-info.java
@@ -1,7 +1,7 @@
 module jakyjty.user.model.Module {
 	
 	requires module jakyjty.user.dao.Module;
-	//requires module exceptions.Module;
+	requires module exception.Module;
 	requires module jakyjty.session.Module;
 	uses IUserManagementService.name;
 }

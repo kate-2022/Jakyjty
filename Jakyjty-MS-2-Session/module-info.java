4cc6339fdf987bdb351d351d99619af4cf0bca80
kate <Katrin.Schaak@gmx.de>
2024-09-17 14:54:26 +0200

    portNo_serviceModule

--- Jakyjty-MS-2-Session/module-info.java
+++ Jakyjty-MS-2-Session/module-info.java
@@ -3,5 +3,5 @@ module jakyjty.session.Module{
 	requires module jakyjty.user.dao.Module;
 	requires module jakyjty.user.model.Module;
 	requires module exception.Module;
-	uses IUserManagementService.name;
+	requires module jakyjty.userservice.Module;
 }

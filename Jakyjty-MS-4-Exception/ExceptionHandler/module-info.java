626412b36641e9cebdd2b44ed3b3615ab8437188
kate <Katrin.Schaak@gmx.de>
2024-09-17 15:02:06 +0200

    portNo_serviceModule

--- Jakyjty-MS-4-Exception/ExceptionHandler/module-info.java
+++ Jakyjty-MS-4-Exception/ExceptionHandler/module-info.java
@@ -3,5 +3,5 @@ module jakyjty.exception.Module{
 	requires module jakyjty.user.dao.Module;
 	requires module jakyjty.user.model.Module;
 	requires module session.Module;
-	uses IUserManagementService.name;
+	requires module jakyjty.userservice.Module;
 }

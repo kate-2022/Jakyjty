5a9dde3e88789e9a876f8818fb1bcfdd27dab33c
kate <Katrin.Schaak@gmx.de>
2025-03-10 12:01:39 +0100

    auth

--- Jakyjty-MS-6--Security/src/main/java/com/jakyjty/security/configuration/SecurityConfigApp.java
+++ Jakyjty-MS-6--Security/src/main/java/com/jakyjty/security/configuration/SecurityConfigApp.java
@@ -1,14 +1,33 @@
 package com.jakyjty.security.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigApp {
+	
+	@Autowired
+	private DataSource datasource;
+	
+	public void authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.jdbcAuthentication()
+			.passwordEncoder(new BCryptPasswordEncoder())
+			.dataSource(datasource)
+			.usersByUsernameQuery("select username, password, enabled from users where username=?") 
+			.authoritiesByUsernameQuery("select username, authority from authorities where username=?");
+			
+	}
 	
 //	@SuppressWarnings("removal")
 	@Bean
